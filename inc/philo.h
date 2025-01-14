32988df74e33b978e39867870983521bcc5c00b2
Tomas Konecny <tkonecny@c3r5s3.42prague.com>
2025-01-14 18:50:48 +0100

    changed my mind about the structure for my data so I changed it

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: tkonecny <tkonecny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 13:22:11 by tkonecny          #+#    #+#             */
-/*   Updated: 2025/01/10 18:41:59 by tkonecny         ###   ########.fr       */
+/*   Updated: 2025/01/14 18:48:05 by tkonecny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,44 +20,33 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+typedef struct s_data
+{
+	int				number_of_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				meals_required;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_lock;
+	int				is_running;
+}					t_data;
 
 typedef struct s_philo
 {
-	pthread_t		thread;
 	int				id;
-	int				eating;
 	int				meals_eaten;
-	size_t			last_meal;
-	int				nf;
-	int				num_times_to_eat;
-	int				*dead;
-	pthread_mutex_t	*r_fork;
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*write_lock;
-	pthread_mutex_t	*dead_lock;
-	pthread_mutex_t	*meal_lock;
-
-}					t_philo;
-
-typedef struct s_data
-{
-	size_t			time_to_die;
-	size_t			time_to_eat;
-	size_t			time_to_sleep;
-	size_t			start_time;
-	int				dead_flag;
-	pthread_mutex_t	dead_lock;
+	size_t			lasr_meal_time;
+	t_data			*data;
+	pthread_t		thread;
 	pthread_mutex_t	meal_lock;
-	pthread_mutex_t	write_lock;
-	t_philo			*philos;
-}					t_data;
+}					t_philosophers;
 
 // Utils
-int	argcheck(char* argv[]);
-int	ft_atoi(const char *nptr);
+int					argcheck(char *argv[]);
+int					ft_atoi(const char *nptr);
 
 // Init
-int	init(char **argv, t_data *data, int argc);
-
+int					init(char **argv, t_data *data, int argc);
 
 #endif
