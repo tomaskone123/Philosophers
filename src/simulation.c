4c833aa6f544595427e1b7d2bcd7080692d94a47
Tomas Konecny <tkonecny@c1r4s7.42prague.com>
2025-01-24 15:14:21 +0100

    balls

--- src/simulation.c
+++ src/simulation.c
@@ -6,7 +6,7 @@
 /*   By: tkonecny <tkonecny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:08:14 by tkonecny          #+#    #+#             */
-/*   Updated: 2025/01/24 15:04:21 by tkonecny         ###   ########.fr       */
+/*   Updated: 2025/01/24 15:07:41 by tkonecny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	philo_lifecycle(t_data *arg)
 	philo = (t_philosophers *)arg;
 	if (philo->input->meals_required == 0)
 		return (NULL);
-	if (philo->input->number_of_philos == 1)
-			
+	
 
 }
